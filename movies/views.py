285faf7540c2fee8946df463e36dbbc58e6e681a
salman <salmancrc9@gmail.com>
2025-07-22 00:51:47 +0500

    Add index template for movie listings and update view to render movies

--- movies/views.py
+++ movies/views.py
@@ -2,15 +2,18 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Movie
 
-# Create your views here.
-
 def index(request):
     movies = Movie.objects.all()
-    output = ', '.join([m.title for m in movies])
+    return render(request, 'movies/index.html', {'movies': movies})
+
+# def index(request):
+#     movies = Movie.objects.all()
+#     output = ', '.join([m.title for m in movies])
     
-    # Example queries:
-    # SELECT * FROM movies_movie
-    # Movie.objects.filer(release_year=1985)
-    # SELECT * FROM movies_movie WHERE release_year=1985
-    # Movie.objects.get(id=1)
-    return HttpResponse(output)
+#     # Example queries:
+#     # SELECT * FROM movies_movie
+#     # Movie.objects.filer(release_year=1985)
+#     # SELECT * FROM movies_movie WHERE release_year=1985
+#     # Movie.objects.get(id=1)
+
+#     return HttpResponse(output)

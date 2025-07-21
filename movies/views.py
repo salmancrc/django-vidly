a25d6905148d53cef95efeb50b00be1a68aa5b0f
salman <salmancrc9@gmail.com>
2025-07-22 00:20:08 +0500

    Update MovieAdmin to include 'daily_rate' in list_display and modify index view to display all movie titles

--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Movie
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Welcome to the Movies page!")
+    movies = Movie.objects.all()
+    output = ', '.join([m.title for m in movies])
+    
+    # Example queries:
+    # SELECT * FROM movies_movie
+    # Movie.objects.filer(release_year=1985)
+    # SELECT * FROM movies_movie WHERE release_year=1985
+    # Movie.objects.get(id=1)
+    return HttpResponse(output)
